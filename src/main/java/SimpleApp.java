74a359697ff7d49cdf6d6f57078f2845081661d5
Suvasish Mondal <suvasishmndl@gmail.com>
2016-07-29 16:48:39 +0530

    Spark App initial commit

---
+++ src/main/java/SimpleApp.java
@@ -0,0 +1,25 @@
+/**
+ * Created by suvasish on 29/7/16.
+ */
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        String logFile = "/home/suvasish/projects/spark-2.0.0-bin-hadoop2.7/README.md";
+        SparkConf conf = new SparkConf().setAppName("Simple Spark Application");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("a"); }
+        }).count();
+
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("b"); }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+    }
+}
